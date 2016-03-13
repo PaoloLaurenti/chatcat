7912ab206afd68e073fd3fefc2d6e0ddeaf0c7c3
Paolo Laurenti <laurentipaolo@gmail.com>
2016-03-13 12:18:45 +0100

    Complete lecture 31: add express application and base routing functionalities

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 'use strict'
 
-console.log("Welcome to Chatcat");
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res, next) => {
+  res.send('<h1>Hello Express!</h1>');
+});
+
+app.get('/dashboard', (req, res, next) => {
+  res.send('<h1>This is the dashboard page</h1>');
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Chatchat running on port:' + app.get('port'));
+});
