aa878de2f95c14fd19b1a00a65ba3ae5a951aaee
Paolo Laurenti <laurentipaolo@gmail.com>
2016-03-16 13:28:37 +0100

    Lecture 32: add example of middleware usage

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+let helloMiddleware = (req, res, next) => {
+  req.hello = "Hello! Bla bla bla!"
+  next();
+};
+
+app.use('/dashboard', helloMiddleware);
+
 app.get('/', (req, res, next) => {
   res.send('<h1>Hello Express!</h1>');
+  console.log(req.hello);
 });
 
 app.get('/dashboard', (req, res, next) => {
-  res.send('<h1>This is the dashboard page</h1>');
+  res.send('<h1>This is the dashboard page. Middleware says: ' +  req.hello + '</h1>');
 });
 
 app.listen(app.get('port'), () => {
