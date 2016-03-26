92af07b01c7f927e85b69547eda0c56e86627860
Paolo Laurenti <laurentipaolo@gmail.com>
2016-03-26 22:12:21 +0100

    Lecture 33: add view rendering

--- server.js
+++ server.js
@@ -4,21 +4,12 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-
-let helloMiddleware = (req, res, next) => {
-  req.hello = "Hello! Bla bla bla!"
-  next();
-};
-
-app.use('/dashboard', helloMiddleware);
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
-  res.send('<h1>Hello Express!</h1>');
-  console.log(req.hello);
-});
-
-app.get('/dashboard', (req, res, next) => {
-  res.send('<h1>This is the dashboard page. Middleware says: ' +  req.hello + '</h1>');
+  res.render('login', {
+    pageTitle: 'My Login Page'
+  });
 });
 
 app.listen(app.get('port'), () => {
