d8ff39beb9c372dd36cd921a52180b9dfcf1181b
Paolo Laurenti <laurentipaolo@gmail.com>
2016-03-28 22:08:04 +0200

    Lecture 37: mounting routes, first part

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,7 +8,7 @@ module.exports = () => {
         res.render('login');
       },
       '/rooms': (req, res, next) => {
-        res.render('romms');
+        res.render('rooms');
       },
       '/chat': (req, res, next) => {
         res.render('chatroom');
@@ -18,4 +18,22 @@ module.exports = () => {
 
     }
   }
+
+  let registerRoutes = (routes, method) => {
+    for (let key in routes) {
+      if (typeof routes[key] === 'object' && routes[key] !== null && !(routes[key] instanceof Array)) {
+        registerRoutes(routes[key], key);
+      } else {
+        if (method === 'get') {
+          router.get(key, routes[key]);
+        } else if (method === 'post') {
+          router.post(key, routes[key]);
+        }
+      }
+    }
+  };
+
+  registerRoutes(routes);
+
+  return router;
 };
