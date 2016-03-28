d907670f203fb1b54c38b4fc1c557ff594589150
Paolo Laurenti <laurentipaolo@gmail.com>
2016-03-28 21:38:38 +0200

    Lecture 35: add router

--- server.js
+++ server.js
@@ -2,16 +2,13 @@
 
 const express = require('express');
 const app = express();
+const chatcat = require('./app');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res, next) => {
-  res.render('login', {
-    pageTitle: 'My Login Page'
-  });
-});
+app.use('/', chatcat.router);
 
 app.listen(app.get('port'), () => {
   console.log('Chatchat running on port:' + app.get('port'));
