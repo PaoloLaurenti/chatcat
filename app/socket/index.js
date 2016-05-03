0fac64ca5d7fe21f1c6a7afa95e0778555d9beac
Paolo Laurenti <laurentipaolo@gmail.com>
2016-05-03 13:52:14 +0200

    Add retrieving of chatrooms list via socket.io

--- app/socket/index.js
+++ app/socket/index.js
@@ -2,7 +2,22 @@
 
 module.exports = (io, app) => {
   let allRooms = app.locals.chatrooms;
+
+  allRooms.push({
+    room: 'Good Food',
+    roomId: '0001',
+    users: []
+  });
+
+  allRooms.push({
+    room: 'Cloud Computing',
+    roomId: '0002',
+    users: []
+  });
+
   io.of('/roomsList').on('connection', socket => {
-    console.log('Socket.io connected to client!');
+    socket.on('getChatRooms', () => {
+      socket.emit('chatRoomsList', JSON.stringify(allRooms));
+    });
   });
 };
